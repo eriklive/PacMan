fc958eaac6e18144aa85cb60cc98f12d07086617
eriklive <eriklive2@gmail.com>
2018-07-29 15:52:14 -0300

    Adicionada font 'PacMan'

--- assets/Fonts.java
+++ assets/Fonts.java
@@ -5,9 +5,11 @@ import java.io.File;
 
 public abstract class Fonts{
 	public static void load(){
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+
 		try {
-		     GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		     ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("PressStart2P.ttf")));
+		    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("PressStart2P.ttf")));
+		    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("pacfont.ttf")));
 		} catch (Exception e) {
 		    System.out.println("Font not loaded: " + e);
 		}
